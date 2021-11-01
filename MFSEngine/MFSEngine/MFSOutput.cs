fc4b929f60e1c010adeaaa30a2a8a1402aef9a05
Tanmay Seth <kushal.seth2010@gmail.com>
2021-11-01 09:56:55 +0530

    Changed constructor parameters of MFSOutput
    Passed more parameters of MFSOutput, will do open closed principle further.

--- MFSEngine/MFSEngine/MFSOutput.cs
+++ MFSEngine/MFSEngine/MFSOutput.cs
@@ -9,19 +9,23 @@ namespace MFSEngine
     class MFSOutput
     {
         private LinkedList<int> mutualFriends;
+        private int source;
+        private Dictionary<int, Person> people;
         BFS _bfs;
-        public MFSOutput(BFS bFS)
+        public MFSOutput(BFS bFS,int s, Dictionary<int, Person> people)
         {
             
             mutualFriends = bFS!=null ? bFS._mutualFriends: throw new NullReferenceException();
             this._bfs = bFS;
+            this.source = s;
+            this.people = people;
         }
 
-        public void mutualFriendsList(int s, Dictionary<int, Person> people)
+        public void mutualFriendsList()
         {
 
-            _bfs.BFSTraversal(s);
-            foreach (var item in mutualFriends)
+            _bfs.BFSTraversal(this.source);
+            foreach (var item in this.mutualFriends)
             {
                 Console.Write(item + " ");
             }
