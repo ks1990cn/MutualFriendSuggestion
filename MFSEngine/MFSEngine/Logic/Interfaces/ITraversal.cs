f865c9d168c3fc6bc925f8d60e7f0d7829445412
HeyKshitij <ksharma860@gmail.com>
2021-11-05 10:39:00 +0000

    missed this guy

--- MFSEngine/MFSEngine/Logic/Interfaces/ITraversal.cs
+++ MFSEngine/MFSEngine/Logic/Interfaces/ITraversal.cs
@@ -5,8 +5,8 @@ using MFSEngine.Models;
 
 namespace MFSEngine.Logic
 {
-public interface ITraversal
-{
-     IEnumerable<int> Traverse(MFSGraph g, int endPoint);
-}
+    public interface ITraversal
+    {
+        IEnumerable<int> Traverse(MFSGraph g, int endPoint);
+    }
 }
