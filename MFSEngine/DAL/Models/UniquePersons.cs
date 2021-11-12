23b17474c329e57f31c5b346d4f1033d974a14d9
Tanmay Seth <kushal.seth2010@gmail.com>
2021-11-13 01:23:07 +0530

    Removed DAL

--- MFSEngine/DAL/Models/UniquePersons.cs
+++ MFSEngine/DAL/Models/UniquePersons.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,13 @@ namespace DAL.Models
     class UniquePersons
     {
         ///Name
+        public string Name { get; set; }
+        
+        ///Unique Id
+        public int UniqueId { get; set; }
+#nullable enable
         ///Location
-        ///Friends
+        public string? Location { get; set; }
+#nullable disable
     }
 }
